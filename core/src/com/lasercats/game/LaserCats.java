a057728d877948cc8789ec2f3b69758806825d5d
dorukkolcak <dorukkolcak@gmail.com>
2024-02-28 16:49:37 +0300

    Changed background color.

--- core/src/com/lasercats/game/LaserCats.java
+++ core/src/com/lasercats/game/LaserCats.java
@@ -17,7 +17,7 @@ public class LaserCats extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(1, 1, 1, 1);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
