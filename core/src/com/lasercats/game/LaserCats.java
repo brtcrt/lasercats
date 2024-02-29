e6d1103b0c41b2f225414d78a951af806bc17ae7
brtcrt <bartuturan05@gmail.com>
2024-02-29 17:21:16 +0300

    Added a simple walking animation

--- core/src/com/lasercats/game/LaserCats.java
+++ core/src/com/lasercats/game/LaserCats.java
@@ -6,6 +6,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.math.Rectangle;
 
@@ -15,16 +18,40 @@ public class LaserCats extends ApplicationAdapter {
 	private Texture img;
 	private Rectangle cat;
 	private OrthographicCamera camera;
+	private Texture whiteCat;
+	private Animation<TextureRegion> idleCat;
+	private Animation<TextureRegion> walkCat;
+	private float stateTime;
+	private boolean walking;
+	private int direction;
+	boolean changedDirection;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("1.png");
+		whiteCat =  new Texture("WhiteCat.png");
+		TextureRegion[][] tmp = TextureRegion.split(whiteCat, 32, 32);
+		TextureRegion[] idleFrames = new TextureRegion[2];
+		TextureRegion[] walkFrames = new TextureRegion[2];
+		walkFrames[0] = tmp[1][0];
+		walkFrames[1] = tmp[1][1];
+
+		idleFrames[0] = tmp[0][0];
+		idleFrames[1] = tmp[0][1];
+
+		idleCat = new Animation<TextureRegion>(0.14f, idleFrames);
+		walkCat = new Animation<TextureRegion>(0.14f, walkFrames);
+
 		cat = new Rectangle();
 		cat.x = 32;
 		cat.y = 32;
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 256, 256);
+
+		stateTime = 0f;
+		walking = false;
+		direction = -1;
 	}
 
 	@Override
@@ -33,28 +60,50 @@ public class LaserCats extends ApplicationAdapter {
 
 		camera.update();
 
-		batch.setProjectionMatrix(camera.combined);
-		batch.begin();
-		batch.draw(img, cat.x, cat.y);
-		batch.end();
 
 		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
 			cat.x += 50 * Gdx.graphics.getDeltaTime();
+			direction = 1;
+			walking = true;
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
 			cat.x -= 50 * Gdx.graphics.getDeltaTime();
+			direction = -1;
+			walking = true;
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
 			cat.y += 50 * Gdx.graphics.getDeltaTime();
+			walking = true;
 		}
 
 		if (Gdx.input.isKeyPressed(Input.Keys.S)) {
 			cat.y -= 50 * Gdx.graphics.getDeltaTime();
+			walking = true;
 		}
 
 
+		stateTime += Gdx.graphics.getDeltaTime();
+		TextureRegion currFrame;
+		if (walking) {
+			currFrame = walkCat.getKeyFrame(stateTime, true);
+			
+		} else {
+			currFrame = idleCat.getKeyFrame(stateTime, true);
+		}
+
+		Sprite catSprite = new Sprite(currFrame);
+		if(direction == 1) {
+			catSprite.flip(true, false);
+			changedDirection = false;
+		}
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(catSprite, cat.x, cat.y);
+		batch.end();
+
+		walking = false;
 
 	}
 	
@@ -62,5 +111,6 @@ public class LaserCats extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+		whiteCat.dispose();
 	}
 }
