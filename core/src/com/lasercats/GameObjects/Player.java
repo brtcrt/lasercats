9d2276d938e8e654d3d6d9c161b755762a92431b
dorukkolcak <dorukkolcak@gmail.com>
2024-03-01 14:55:01 +0300

    Deleted some unused code and added a note about the Empty class

--- core/src/com/lasercats/GameObjects/Player.java
+++ core/src/com/lasercats/GameObjects/Player.java
@@ -2,15 +2,10 @@ package com.lasercats.GameObjects;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.math.Rectangle;
-
 
 public class Player extends Empty {
     private Texture playerTexture;
--- core/src/com/lasercats/game/LaserCats.java
+++ core/src/com/lasercats/game/LaserCats.java
@@ -6,33 +6,23 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.lasercats.GameObjects.Player;
-import com.badlogic.gdx.math.Rectangle;
 
 
 public class LaserCats extends ApplicationAdapter {
 	private SpriteBatch batch;
-	private Texture img;
-	private Rectangle cat;
+	private Texture catTexture;
 	private OrthographicCamera camera;
-	private Texture whiteCat;
-	private Animation<TextureRegion> idleCat;
-	private Animation<TextureRegion> walkCat;
-	private float stateTime;
-	private boolean walking;
-	private int direction;
-	boolean changedDirection;
-	private Player player;
+	private Texture catAnimationSheet;
+	private Player cat;
 
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture(Gdx.files.internal("1.png"));
-		whiteCat =  new Texture(Gdx.files.internal("WhiteCat.png"));
+		catTexture = new Texture(Gdx.files.internal("Cat.png"));
+		catAnimationSheet =  new Texture(Gdx.files.internal("CatAnimationSheet.png"));
 
 		// TextureRegion[][] tmp = TextureRegion.split(whiteCat, 32, 32);
 		// TextureRegion[] idleFrames = new TextureRegion[2];
@@ -56,7 +46,7 @@ public class LaserCats extends ApplicationAdapter {
 		// stateTime = 0f;
 		// walking = false;
 		// direction = -1;
-		this.player = new Player(32, 32, 32, 32, img, whiteCat);
+		this.cat = new Player(32, 32, 32, 32, catTexture, catAnimationSheet);
 	}
 
 	@Override
@@ -68,29 +58,29 @@ public class LaserCats extends ApplicationAdapter {
 
 		// TODO This if chain is dumb change this ~brtcrt
 		if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-			player.move(Input.Keys.D);
+			cat.move(Input.Keys.D);
 		} if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-			player.move(Input.Keys.A);
+			cat.move(Input.Keys.A);
 		} if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-			player.move(Input.Keys.W);
+			cat.move(Input.Keys.W);
 		} if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-			player.move(Input.Keys.S);
+			cat.move(Input.Keys.S);
 		} 
-		Sprite catSprite = player.getSprite();
+		Sprite catSprite = cat.getSprite();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(catSprite, player.x, player.y, 128, 128);
+		batch.draw(catSprite, cat.x, cat.y, 128, 128);
 		batch.end();
 
-		player.move(-1);
+		cat.move(-1);
 	}
 
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
-		whiteCat.dispose();
+		catTexture.dispose();
+		catAnimationSheet.dispose();
 	}
 }
